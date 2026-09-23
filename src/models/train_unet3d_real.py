5565bd6eed2a455dd965080b6811b86cbf545aed
keerthweb-1519 <yashodhakeerthi19@gmail.com>
2026-09-23 21:57:16 +0530

    updated train_unet3d_real file

--- src/models/train_unet3d_real.py
+++ src/models/train_unet3d_real.py
@@ -26,11 +26,6 @@ sys.path.insert(0, str(PROJECT_ROOT))
 
 from src.models.unet3d_model import UNet3D
 
-
-# ================================================================
-# SETTINGS
-# ================================================================
-
 BATCH_SIZE = 16
 EPOCHS = 30
 LEARNING_RATE = 1e-3
