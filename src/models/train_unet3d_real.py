02b3c6a1434494671b944b39ba82fc38ed835437
keerthweb-1519 <yashodhakeerthi19@gmail.com>
2026-09-23 21:57:34 +0530

    updated train_unet3d_real file

--- src/models/train_unet3d_real.py
+++ src/models/train_unet3d_real.py
@@ -1,16 +1,11 @@
 import json
 import sys
 from pathlib import Path
-
 import numpy as np
 import pandas as pd
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
-
-# ================================================================
-# PROJECT PATHS
-# ================================================================
 
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
 
